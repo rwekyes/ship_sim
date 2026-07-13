ac839b777fbd078b1ee0a81fa456c6562df1a522
Tall Manderson <rwekyes@gmail.com>
2026-07-12 20:10:08 -0700

    propagate_mean_anomaly

--- crates/sim-core/src/orbits.rs
+++ crates/sim-core/src/orbits.rs
@@ -1,5 +1,6 @@
 use hifitime::Epoch;
 use serde::{Deserialize, Serialize};
+use std::f64::consts::TAU;
 /// Orbital Elements struct
 #[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
 pub struct OrbitalElements {
@@ -11,10 +12,19 @@ pub struct OrbitalElements {
     pub inclination: f64,
     /// Longitude of ascending node (rad)
     pub ascending_node: f64,
-    /// Argument of periapsis
+    /// Argument of periapsis (rad)
     pub arg_periapsis: f64,
     /// Mean anomaly at epoch (rad)
     pub mean_anomaly_epoch: f64,
     /// Reference time at which mean_anomaly_epoch applies
     pub epoch: Epoch,
+}
+/// Propagate the mean anomaly
+///
+/// μ is m³/s², a³ is m³, so μ/a³ is 1/s² and its square root is rad/s.
+/// mu can be retrieved from bodies.rs per major body or supplied raw
+/// dt is seconds since elements.epoch
+pub fn propagate_mean_anomaly(elements: &OrbitalElements, mu: f64, dt: f64) -> f64 {
+    (((mu / elements.semi_major_axis.powi(3)).sqrt() * dt) + elements.mean_anomaly_epoch)
+        .rem_euclid(TAU)
 }
