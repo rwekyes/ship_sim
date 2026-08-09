8e093c251887202920cd2ce4eafd63623b0c2e27
Tall Manderson <rwekyes@gmail.com>
2026-08-08 18:49:16 -0700

    chore: initial scaffolding for burn integrator

---
+++ crates/sim-core/src/integrate.rs
@@ -0,0 +1,13 @@
+use crate::vectors::StateVector;
+use glam::DVec3;
+
+pub fn integrate<F>(state: StateVector, t0: f64, dt_total: f64, substep: f64, accel: F) -> StateVector
+where F: Fn(f64, &StateVector) -> DVec3 {
+
+}
+
+#[cfg(test)]
+mod tests {
+
+}
+
