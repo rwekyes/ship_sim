4864d274f91cfed306f82bb61f7c8bf9e8703478
Tall Manderson <rwekyes@gmail.com>
2026-08-08 20:09:09 -0700

    chore: two_body

--- crates/sim-core/src/integrate.rs
+++ crates/sim-core/src/integrate.rs
@@ -1,13 +1,21 @@
 use crate::vectors::StateVector;
 use glam::DVec3;
 
-pub fn integrate<F>(state: StateVector, t0: f64, dt_total: f64, substep: f64, accel: F) -> StateVector
-where F: Fn(f64, &StateVector) -> DVec3 {
-
+pub fn two_body(mu: f64, state: &StateVector) -> DVec3 {
+    let r = state.position;
+    -mu * r / r.length().powi(3)
+}
+pub fn integrate<F>(
+    state: StateVector,
+    t0: f64,
+    dt_total: f64,
+    substep: f64,
+    accel: F,
+) -> StateVector
+where
+    F: Fn(f64, &StateVector) -> DVec3,
+{
 }
 
 #[cfg(test)]
-mod tests {
-
-}
-
+mod tests {}
