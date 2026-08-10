b696d65720d03662494d6a1207bc215d51b41d7c
Tall Manderson <rwekyes@gmail.com>
2026-08-10 12:18:10 -0700

    chore: burns.rs initial scaffold and serialization test

--- crates/sim-core/src/burns.rs
+++ crates/sim-core/src/burns.rs
@@ -1,1 +1,106 @@
+//! Burn related data structures and logic
+//! Boundary between Epoch and seconds
 
+use glam::DVec3;
+use hifitime::Epoch;
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum BurnError {
+    #[error("Direction {0} must be a nonzero, finite vector")]
+    InvalidDirection(DVec3),
+    #[error("Duration {0} must be finite and non-negative")]
+    InvalidDuration(f64),
+    #[error("Acceleration {0} must be finite and non-negative")]
+    InvalidAccel(f64),
+}
+
+/// Burn struct
+#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
+#[serde(try_from = "BurnRepr")]
+pub struct Burn {
+    /// Start of burn
+    start: Epoch,
+    /// Duration of burn (seconds)
+    duration: f64,
+    /// Acceleration (meters per second squared)
+    accel: f64,
+    /// Direction - unit vector, fixed inertial heading, normalized
+    direction: DVec3,
+}
+
+impl Burn {
+    /// Constructor returns a BurnError if any parameters are negative, non-finite, or if direction cannot be normalized.
+    pub fn new(
+        start: Epoch,
+        duration: f64,
+        accel: f64,
+        direction: DVec3,
+    ) -> Result<Self, BurnError> {
+        let direction = direction
+            .try_normalize()
+            .ok_or(BurnError::InvalidDirection(direction))?;
+        if !duration.is_finite() || duration < 0.0 {
+            return Err(BurnError::InvalidDuration(duration));
+        } else if !accel.is_finite() || accel < 0.0 {
+            return Err(BurnError::InvalidAccel(accel));
+        }
+        Ok(Self {
+            start,
+            duration,
+            accel,
+            direction,
+        })
+    }
+    pub fn start(self) -> Epoch {
+        self.start
+    }
+
+    pub fn duration(self) -> f64 {
+        self.duration
+    }
+
+    pub fn accel(self) -> f64 {
+        self.accel
+    }
+
+    pub fn direction(self) -> DVec3 {
+        self.direction
+    }
+}
+
+#[derive(Deserialize)]
+struct BurnRepr {
+    start: Epoch,
+    duration: f64,
+    accel: f64,
+    direction: DVec3,
+}
+
+impl TryFrom<BurnRepr> for Burn {
+    type Error = BurnError;
+    fn try_from(repr: BurnRepr) -> Result<Self, Self::Error> {
+        Burn::new(repr.start, repr.duration, repr.accel, repr.direction)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::time::J2000;
+    #[test]
+    fn serialize_round_trip() {
+        let burn = Burn::new(*J2000, 60.0, 100.0, DVec3::new(0.11, -4.2, 0.77777)).unwrap();
+        let string = serde_json::to_string(&burn).unwrap();
+        let new_burn: Burn = serde_json::from_str(&string).unwrap();
+        assert_eq!(burn.start(), new_burn.start());
+        assert_eq!(burn.duration(), new_burn.duration());
+        assert_eq!(burn.accel(), new_burn.accel());
+        assert!(
+            burn.direction().distance(new_burn.direction()) < 1e-15,
+            "burn direction difference is {}, greater than tolerance 1e-15",
+            burn.direction().distance(new_burn.direction())
+        );
+    }
+}
