073c18a9bc6c332e21a01838bd570050d59f2924
Tall Manderson <rwekyes@gmail.com>
2026-08-28 22:07:47 -0700

    chore: FlightPlan scaffold

--- crates/sim-core/src/plan.rs
+++ crates/sim-core/src/plan.rs
@@ -1,7 +1,17 @@
+//! Flight plan containers
+//! Serves both the planner and the UI
+use crate::burns::Burn;
+/// May add variants for game reasons, like Waypoint
 enum Maneuver{
-
+    Burn(Burn),
 }
 
 struct FlightPlan{
+    maneuvers: Vec<Maneuver>,
+}
 
+impl FlightPlan {
+    pub fn maneuvers(&self) -> impl Iterator<Item = &Maneuver> {
+        self.maneuvers.iter()
+    }
 }
